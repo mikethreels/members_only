56fc6f362c96437399be2eef4a67ecb8e5c71c13
mikethreels <michaelthreels@gmail.com>
2020-07-15 23:49:04 +0200

    adjusted the routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, only: [:new, :create]
+    # before_action :authenticate_user!, only: [:new, :create]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:new, :create, :index]
+  resources :posts
   root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
